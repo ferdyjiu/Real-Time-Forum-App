61cfb650c06bac8ce691f92e66495d6f9b0ebd78
ferdy jiu <ferdyjiu@ferdys-MacBook-Pro.local>
2019-03-05 22:11:01 +0700

    Relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,17 @@
 
 namespace App\Model;
 
+use App\User;
+use App\Model\Question;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
     //
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
