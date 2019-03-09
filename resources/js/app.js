083f1b00cf5504cbd98ac18ce8ac9956b63bfd00
ferdy jiu <ferdyjiu@ferdys-MacBook-Pro.local>
2019-03-09 16:33:44 +0700

    vuetify, vue router, and intial components

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,14 +20,21 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app-home', require('./components/AppHome.vue').default);
 
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import router from './router/router.js'
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
